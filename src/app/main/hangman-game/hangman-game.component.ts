5949ed738399961f5b7971a290739bb6c7ebf903
Konrad.Parypa <konrad.parypa@asseco.pl>
2020-11-16 22:08:21 +0100

    style changes, only-letter directive change

--- src/app/main/hangman-game/hangman-game.component.ts
+++ src/app/main/hangman-game/hangman-game.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {ObjectOfLettersPattern} from '../../shared/models/objectOfLettersPattern';
 
 @Component({
   selector: 'app-hangman-game',
@@ -7,18 +8,78 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./hangman-game.component.sass']
 })
 export class HangmanGameComponent implements OnInit {
+  @ViewChild('canvas', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+  private ctx: CanvasRenderingContext2D;
 
   stringsArray: [];
   randomizedStrings: any;
+  letters: Array<ObjectOfLettersPattern>;
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.stringsArray = this.route.snapshot.data.tablicaHasel.ArrayOfStrings;
     this.randomizedStrings = this.randomizeFiveStrings();
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+
+    this.drawCanvas();
+
+    this.ctx.beginPath();
+    // -> głowa
+    this.ctx.arc(200, 100, 50, 0, 2 * Math.PI);
+
+    // -> tułów
+    this.ctx.moveTo(200, 150);
+    this.ctx.lineTo(200, 270);
+
+    // -> nogi
+    this.ctx.moveTo(200, 270);
+    this.ctx.lineTo(150, 350);
+
+    this.ctx.moveTo(200, 270);
+    this.ctx.lineTo(250, 350);
+
+    // -> rece
+    this.ctx.moveTo(200, 210);
+    this.ctx.lineTo(130, 180);
+
+    this.ctx.moveTo(200, 210);
+    this.ctx.lineTo(280, 180);
+
+    this.ctx.stroke();
+
+    this.makeLetters();
+  }
+
+  drawCanvas() {
+    this.ctx.beginPath();
+    this.ctx.moveTo(200, 50);
+    this.ctx.lineTo(200, 10);
+    this.ctx.lineTo(50, 10);
+    this.ctx.lineTo(50, 400);
+    this.ctx.stroke();
+  }
+
+  makeLetters() {
+    debugger;
+    const word = ('abcdefghijklmnopqrstuvwxyz');
+    this.letters = word.split('').map((char) => {
+      return { name: char, chosen: false };
+    });
   }
 
   sendTypedLetter(val) {
 
+  }
+
+  checkLetter(val) {
+    if (val) {
+      this.letters.find( (x: ObjectOfLettersPattern) => {
+        if (x.name === val.name) {
+          x.chosen = true;
+        }
+      });
+    }
   }
 
   private randomizeFiveStrings() {
---
+++ src/app/shared/models/objectOfLettersPattern.ts
@@ -0,0 +1,4 @@
+export class ObjectOfLettersPattern {
+  name: string;
+  chosen: boolean;
+}
